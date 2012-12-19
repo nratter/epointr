02ab6c9d4990fe180772220b928b99dd9460313f
Nick Rattermann <nrattermann@gmail.com>
2012-12-19 01:57:34 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Epointr::Application.config.secret_token = 'c3ca4b2dabd55ca236c268939249ff95deb37bf1487439095eb559145de7cdb312e71dcaeb4649e52b3b4599db4a8f631c211e6f1ddc1ef4e27767a45bb21939'
